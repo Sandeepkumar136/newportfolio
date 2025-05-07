f5da188ae608f13507dec959aa60b9c55773a99c
Sandeep Kumar <Ksundip750@gmail.com>
2025-05-07 22:28:29 +0530

    Add GItData component and update References to render it

---
+++ src/Contents/GItData.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GItData = () => {
+    
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default GItData
--- src/components/References.js
+++ src/components/References.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import GItData from '../Contents/GItData';
 
 const References = () => {
   return (
     <div>
-      References
+      <GItData/>
     </div>
   )
 }
