03bf2bd7cdead2a89d9b838fa44fd7636ae0239a
Sandeep Kumar <Ksundip750@gmail.com>
2025-05-14 22:42:01 +0530

    fix: correct spelling of "Certificates" in Achievements component

--- src/components/Achievements.js
+++ src/components/Achievements.js
@@ -1,9 +1,46 @@
 import React from 'react'
 
 const Achievements = () => {
+  const Certificates =[
+    {
+      certificate: 'DCA',
+      subititle: 'OXFORD Education',
+      date: '10-9-2019',
+    },
+    {
+      certificate: 'TallyERP 9',
+      subititle: 'OXFORD Education',
+      date: '5-5-2020',
+    },
+    {
+      certificate: 'Master in EXCEL',
+      subititle: 'OXFORD Education',
+      date: '7-4-2020'
+    },
+    {
+      certificate: 'Amazon Machie',
+      subititle: "Amazon Web Services",
+      date: '14-9-2024'
+    },
+    {
+      certificate: 'Cloud for CEOs',
+      subititle: "Amazon Web Services",
+      date: '13-3-2024'
+    },
+    {
+      certificate: 'AWS Cloud Quest',
+      subititle: "Amazon Web Services",
+      date: "15-3-24"
+    },
+    {
+      
+    }
+  ]
   return (
     <div>
-      Achievements
+      <div className="certificates">
+        Ceritificates
+      </div>
     </div>
   )
 }
