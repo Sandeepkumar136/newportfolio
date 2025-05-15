9ad118c8cd49e0f37b123217f6e681e6988401c4
Sandeep Kumar <Ksundip750@gmail.com>
2025-05-15 22:37:34 +0530

    fix: correct spelling of "Certificates" and improve formatting in Achievements component

--- src/components/Achievements.js
+++ src/components/Achievements.js
@@ -1,48 +1,96 @@
-import React from 'react'
+import React from "react";
 
 const Achievements = () => {
-  const Certificates =[
+  const Certificates = [
     {
-      certificate: 'DCA',
-      subititle: 'OXFORD Education',
-      date: '10-9-2019',
+      certificate: "DCA",
+      subititle: "OXFORD Education",
+      date: "10-9-2019",
     },
     {
-      certificate: 'TallyERP 9',
-      subititle: 'OXFORD Education',
-      date: '5-5-2020',
+      certificate: "TallyERP 9",
+      subititle: "OXFORD Education",
+      date: "5-5-2020",
     },
     {
-      certificate: 'Master in EXCEL',
-      subititle: 'OXFORD Education',
-      date: '7-4-2020'
+      certificate: "Master in EXCEL",
+      subititle: "OXFORD Education",
+      date: "7-4-2020",
     },
     {
-      certificate: 'Amazon Machie',
+      certificate: "Amazon Machie",
       subititle: "Amazon Web Services",
-      date: '14-9-2024'
+      date: "14-9-2024",
     },
     {
-      certificate: 'Cloud for CEOs',
+      certificate: "Cloud for CEOs",
       subititle: "Amazon Web Services",
-      date: '13-3-2024'
+      date: "13-3-2024",
     },
     {
-      certificate: 'AWS Cloud Quest',
+      certificate: "AWS Cloud Quest",
       subititle: "Amazon Web Services",
-      date: "15-3-24"
+      date: "15-3-2024",
     },
     {
-      
-    }
-  ]
+      certificate: "Master in Scss",
+      subititle: "W3S School",
+      date: "16-4-2024",
+    },
+    {
+      certificate: "Prevent RSI",
+      subititle: "Page Industries Limited",
+      date: "20-4-2025",
+    },
+    {
+      certificate: "Jockey Life Mens",
+      subititle: "Page Industries Limited",
+      date: "14-02-2025",
+    },
+    {
+      certificate: "New Arrival and Changes M11",
+      subititle: "Page Industries Limited",
+      date: "06-04-2025",
+    },
+    {
+      certificate: "Intro to Page by Ganesh VS",
+      subititle: "Page Industries Limited",
+      date: "07-01-2025",
+    },
+    {
+      certificate: "New Arrival and Changes M09",
+      subititle: "Page Industries Limited",
+      date: "03-01-2025",
+    },
+    {
+      certificate: "M12 Boxers Shorts Refreshers",
+      subititle: "Page Industries Limited",
+      date: "14-02-2024",
+    },
+    {
+      certificate: "New Arrivals M2 2022",
+      subititle: "Page Industries Limited",
+      date: "25-06-2022",
+    },
+  ];
   return (
     <div>
       <div className="certificates">
-        Ceritificates
+        {Certificates.map((e, i) => (
+          <div key={i} className="cert-container">
+            <div className="uni-logo-contain-c">
+              <div className="logo-uni-overlay-c">
+                <i className="bx bxs-trophy"></i>
+              </div>
+              <div className="uni-title-c">{e.certificate}</div>
+            </div>
+            <h3 className="subtitle-cert">{e.subititle}</h3>
+            <div className="certi-date">{e.date}</div>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Achievements
+export default Achievements;
