cf93b177cdf28160fc919278a813d1713042cd29
Sandeep Kumar <Ksundip750@gmail.com>
2025-06-27 22:44:30 +0530

    Enhance active state styles for navigation items and side items
    - Added background color for active navigation items in light and dark modes.
    - Updated styles for active state of side items to improve visibility.
    - Ensured consistent styling for various components in dark mode.
    - Cleaned up whitespace and formatting in the SCSS file for better readability.

--- src/Contents/Navbar.js
+++ src/Contents/Navbar.js
@@ -18,17 +18,17 @@ const Navbar = () => {
                 </Link>
                 <ul className="nav-list">
                     <NavLink to="services" className={({isActive})=> isActive ? "nav-item active": "nav-item" } >services</NavLink>
-                    <NavLink to="education" className="nav-item">education</NavLink>
-                    <NavLink to="contributors" className="nav-item">contributors</NavLink>
-                    <NavLink to="references" className="nav-item">projects</NavLink>
-                    <NavLink to="moreinfo" className="nav-item">more information</NavLink>
+                    <NavLink to="education" className={({isActive})=> isActive ? "nav-item active": "nav-item" }>education</NavLink>
+                    <NavLink to="contributors" className={({isActive})=> isActive ? "nav-item active": "nav-item" }>contributors</NavLink>
+                    <NavLink to="references" className={({isActive})=> isActive ? "nav-item active": "nav-item" }>projects</NavLink>
+                    <NavLink to="moreinfo" className={({isActive})=> isActive ? "nav-item active": "nav-item" }>more information</NavLink>
                 </ul>
             </div>
             <div className="rhs-contain">
                 <ul className="nav-list-rh">
-                    <NavLink to='resume' className="nav-items-rh">resume</NavLink>
-                    <NavLink to='achievements' className="nav-items-rh">Achievements</NavLink>
-                    <NavLink to='about' className="nav-items-rh">about us</NavLink>
+                    <NavLink to='resume' className={({isActive})=> isActive ? "nav-items-rh active": "nav-items-rh" } >resume</NavLink>
+                    <NavLink to='achievements' className={({isActive})=> isActive ? "nav-items-rh active": "nav-items-rh" } >Achievements</NavLink>
+                    <NavLink to='about' className={({isActive})=> isActive ? "nav-items-rh active": "nav-items-rh" } >about us</NavLink>
                     <li onClick={toggleDarkMode} className="nav-items-rh-th"><i className={`bx bx-${darkMode ? 'moon': 'sun'}`}></i></li>
                 </ul>
                 <i onClick={ToggleSidebar} className={`toggle-sidebar bx ${isSidebarOpen ? "bx-x": "bx-menu"}`}></i>
@@ -36,18 +36,18 @@ const Navbar = () => {
         </nav>
       <aside className={`sidebar ${isSidebarOpen ? "open": ''}`}>
         <ul className="side-list">
-            <NavLink  onClick={ToggleSidebar} to='services' className="side-item"><i className="side-icon bx bx-book-content"></i> Services</NavLink>
-            <NavLink  onClick={ToggleSidebar} to='education' className="side-item"><i className="side-icon bx bxs-graduation"></i> education</NavLink>
-            <NavLink  onClick={ToggleSidebar} to='contributors' className="side-item"><i className="side-icon bx bx-group"></i> contributors</NavLink>
-            <NavLink  onClick={ToggleSidebar} to='references' className="side-item"><i className="side-icon bx bx-detail"></i> projects</NavLink>
-            <NavLink  onClick={ToggleSidebar} to='resume' className="side-item"><i className="side-icon bx bxs-file-pdf"></i> resume</NavLink>
-            <NavLink  onClick={ToggleSidebar} to='achievements' className="side-item"><i className="side-icon bx bx-trophy"></i> Achievements</NavLink>
-            <NavLink  onClick={ToggleSidebar} to='moreinfo' className="side-item"><i className="side-icon bx bx-dots-horizontal-rounded"></i> more information</NavLink>
-            <NavLink  onClick={ToggleSidebar} to='about' className="side-item"><i className="side-icon bx bx-user"></i> about us</NavLink>
+            <NavLink  onClick={ToggleSidebar} to='services' className={({isActive})=> isActive ? "side-item active": "side-item" }  ><i className="side-icon bx bx-book-content"></i> Services</NavLink>
+            <NavLink  onClick={ToggleSidebar} to='education' className={({isActive})=> isActive ? "side-item active": "side-item" }  ><i className="side-icon bx bxs-graduation"></i> education</NavLink>
+            <NavLink  onClick={ToggleSidebar} to='contributors' className={({isActive})=> isActive ? "side-item active": "side-item" }  ><i className="side-icon bx bx-group"></i> contributors</NavLink>
+            <NavLink  onClick={ToggleSidebar} to='references' className={({isActive})=> isActive ? "side-item active": "side-item" }  ><i className="side-icon bx bx-detail"></i> projects</NavLink>
+            <NavLink  onClick={ToggleSidebar} to='resume' className={({isActive})=> isActive ? "side-item active": "side-item" }  ><i className="side-icon bx bxs-file-pdf"></i> resume</NavLink>
+            <NavLink  onClick={ToggleSidebar} to='achievements' className={({isActive})=> isActive ? "side-item active": "side-item" }  ><i className="side-icon bx bx-trophy"></i> Achievements</NavLink>
+            <NavLink  onClick={ToggleSidebar} to='moreinfo' className={({isActive})=> isActive ? "side-item active": "side-item" }  ><i className="side-icon bx bx-dots-horizontal-rounded"></i> more information</NavLink>
+            <NavLink  onClick={ToggleSidebar} to='about' className={({isActive})=> isActive ? "side-item active": "side-item" }  ><i className="side-icon bx bx-user"></i> about us</NavLink>
         </ul>
       </aside>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
